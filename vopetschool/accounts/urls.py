a95a055a1d6ce50193d3c4bc551a71d281bc6d11
Mykhailo-Tr <mykhailo.tretiak098@gmail.com>
2025-07-02 14:37:38 +0300

    Add home path to urlpatterns for ProfileView

--- vopetschool/accounts/urls.py
+++ vopetschool/accounts/urls.py
@@ -5,6 +5,7 @@ from .views import (
 )
 
 urlpatterns = [
+    path("", ProfileView.as_view(), name="home"),
     path("register/", RoleSelectView.as_view(), name="register"),
     path("accounts/register/<str:role>/", RegisterView.as_view(), name="register_role"),
     path("login/", CustomLoginView.as_view(), name="login"),

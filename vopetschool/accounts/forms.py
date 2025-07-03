0899e1c8253b7aa4a144c264d476774aa4badf3c
Mykhailo-Tr <mykhailo.tretiak098@gmail.com>
2025-07-03 15:21:29 +0300

    Enhance registration and profile forms: - Update StudentRegisterForm to use ModelChoiceField for school class selection. - Add ModelMultipleChoiceField for TeacherRegisterForm to select teacher groups. - Display teacher groups in profile template. - Introduce custom template tag for widget type detection in forms.

--- vopetschool/accounts/forms.py
+++ vopetschool/accounts/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth.forms import AuthenticationForm
-from .models import User, Student, Teacher, Parent
+from .models import User, Student, Teacher, Parent, ClassGroup, TeacherGroup
 
 
 class RoleChoiceForm(forms.Form):
@@ -39,24 +39,34 @@ class BaseRegisterForm(forms.ModelForm):
 
 
 class StudentRegisterForm(BaseRegisterForm):
-    school_class = forms.CharField(label="Клас")
+    school_class = forms.ModelChoiceField(
+        queryset=ClassGroup.objects.all(),
+        label="Оберіть клас",
+        empty_label="(Не вибрано)"
+    )
 
     def save(self, commit=True):
         user = self.save_user(User.Role.STUDENT, commit=commit)
         if commit:
-            Student.objects.create(user=user, school_class=self.cleaned_data["school_class"])
+            student = Student.objects.create(user=user, school_class=self.cleaned_data["school_class"].name)
+            self.cleaned_data["school_class"].students.add(student)  # додати в групу
         return user
 
 
 class TeacherRegisterForm(BaseRegisterForm):
     subject = forms.CharField(label="Предмет")
+    groups = forms.ModelMultipleChoiceField(
+        queryset=TeacherGroup.objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+        label="Групи вчителів"
+    )
 
     def save(self, commit=True):
         user = self.save_user(User.Role.TEACHER, commit=commit)
         if commit:
-            Teacher.objects.create(user=user, subject=self.cleaned_data["subject"])
+            teacher = Teacher.objects.create(user=user, subject=self.cleaned_data["subject"])
+            teacher.groups.set(self.cleaned_data["groups"])
         return user
-
 
 class ParentRegisterForm(BaseRegisterForm):
     children = forms.ModelMultipleChoiceField(

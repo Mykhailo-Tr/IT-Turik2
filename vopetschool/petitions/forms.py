c7b44d45dbf0625a1bc74c9f74f930adf9b36c7c
Mox1toGH <denisbaluk187@gmail.com>
2025-07-03 22:11:01 +0300

    Refactor petition form and template for improved layout and user experience

--- vopetschool/petitions/forms.py
+++ vopetschool/petitions/forms.py
@@ -6,7 +6,11 @@ class PetitionForm(forms.ModelForm):
         model = Petition
         fields = ["title", "text", "deadline", "level"]
         widgets = {
-            "deadline": forms.DateTimeInput(attrs={"type": "datetime-local"})
+            "deadline": forms.DateTimeInput(attrs={"type": "datetime-local"}),
+            "text": forms.Textarea(attrs={
+                "rows": 5,
+                "class": "form-control rounded-4 shadow-sm"
+            }),
         }
 
     def __init__(self, *args, **kwargs):

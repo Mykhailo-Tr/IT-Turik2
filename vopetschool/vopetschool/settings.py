29d3bfa51206731fc44d07d40e048e8aea8bdffc
Mykhailo-Tr <mykhailo.tretiak098@gmail.com>
2025-07-03 18:45:55 +0300

    Add 'petitions' app to INSTALLED_APPS in settings.py

--- vopetschool/vopetschool/settings.py
+++ vopetschool/vopetschool/settings.py
@@ -38,11 +38,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
     'widget_tweaks',
     "crispy_forms",
     "crispy_bootstrap5",
+    
     'accounts', 
     'voting',
+    'petitions',
 ]
 
 MIDDLEWARE = [

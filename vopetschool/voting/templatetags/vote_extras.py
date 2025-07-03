4a25585f3f0a8a42aa5e3b9be689f36549dde493
Mox1toGH <denisbaluk187@gmail.com>
2025-07-04 00:05:06 +0300

    Add custom template filter to retrieve field type in Django forms

--- vopetschool/voting/templatetags/vote_extras.py
+++ vopetschool/voting/templatetags/vote_extras.py
@@ -5,3 +5,7 @@ register = template.Library()
 @register.filter
 def get_item(dictionary, key):
     return dictionary.get(key)
+
+@register.filter
+def field_type(field):
+    return field.field.__class__.__name__

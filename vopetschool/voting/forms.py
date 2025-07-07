316c6916ba32362ab478739a14fb07f38323955e
Mykhailo-Tr <mykhailo.tretiak098@gmail.com>
2025-07-07 21:47:18 +0300

    refactor voting/forms.py

--- vopetschool/voting/forms.py
+++ vopetschool/voting/forms.py
@@ -1,30 +1,24 @@
-
 from django import forms
-from django.forms import formset_factory, BaseFormSet
+from django.forms import formset_factory
 
-from .models import Vote
+from .models import Vote, VoteOption
 from schoolgroups.models import TeacherGroup, ClassGroup
 from accounts.models import User
+
 
 class VoteForm(forms.Form):
     def __init__(self, vote: Vote, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        options = vote.options.all()
-        choices = [(option.id, option.text) for option in options]
+        choices = [(option.id, option.text) for option in vote.options.all()]
+        field_class = forms.MultipleChoiceField if vote.multiple_choices_allowed else forms.ChoiceField
+        widget = forms.CheckboxSelectMultiple if vote.multiple_choices_allowed else forms.RadioSelect
 
-        if vote.multiple_choices_allowed:
-            self.fields["options"] = forms.MultipleChoiceField(
-                choices=choices,
-                widget=forms.CheckboxSelectMultiple,
-                label="Оберіть один або кілька варіантів"
-            )
-        else:
-            self.fields["options"] = forms.ChoiceField(
-                choices=choices,
-                widget=forms.RadioSelect,
-                label="Оберіть один варіант"
-            )
+        self.fields["options"] = field_class(
+            choices=choices,
+            widget=widget,
+            label="Оберіть варіант(и)"
+        )
 
 
 class VoteCreateForm(forms.ModelForm):
@@ -38,35 +32,32 @@ class VoteCreateForm(forms.ModelForm):
         widgets = {
             "start_date": forms.DateTimeInput(attrs={"type": "datetime-local"}),
             "end_date": forms.DateTimeInput(attrs={"type": "datetime-local"}),
-            "participants": forms.CheckboxSelectMultiple(attrs={"size": 5}),
-            "teacher_groups": forms.CheckboxSelectMultiple(attrs={"size": 5}),
-            "class_groups": forms.CheckboxSelectMultiple(attrs={"size": 5}),
+            "participants": forms.CheckboxSelectMultiple(),
+            "teacher_groups": forms.CheckboxSelectMultiple(),
+            "class_groups": forms.CheckboxSelectMultiple(),
         }
 
     def __init__(self, *args, **kwargs):
-        user = kwargs.pop("user", None)  # Отримаємо автора
+        user = kwargs.pop("user", None)
         super().__init__(*args, **kwargs)
 
-        qs = User.objects.all()
-        if user:
-            qs = qs.exclude(id=user.id)  # Прибрати автора зі списку
-
-        self.fields["participants"].queryset = qs
+        self.fields["participants"].queryset = User.objects.exclude(id=user.id) if user else User.objects.all()
         self.fields["teacher_groups"].queryset = TeacherGroup.objects.all()
         self.fields["class_groups"].queryset = ClassGroup.objects.all()
 
-        self.fields["participants"].required = False
-        self.fields["teacher_groups"].required = False
-        self.fields["class_groups"].required = False
-        
-        
-
-
+        for field in ("participants", "teacher_groups", "class_groups"):
+            self.fields[field].required = False
 
 
 class VoteOptionForm(forms.Form):
     text = forms.CharField(label="Варіант", max_length=200)
-    is_correct = forms.BooleanField(required=False, label="Правильний (для тестів)")
+    is_correct = forms.BooleanField(required=False, label="Правильний")
 
 
-VoteOptionFormSet = formset_factory(VoteOptionForm, extra=0, min_num=2, validate_min=True, can_delete=True,)
+VoteOptionFormSet = formset_factory(
+    VoteOptionForm,
+    extra=0,
+    min_num=2,
+    validate_min=True,
+    can_delete=True,
+)

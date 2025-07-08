f1ce3956a3dc63a6ca2c0dcf519dc14a2457c2fa
Mykhailo-Tr <mykhailo.tretiak098@gmail.com>
2025-07-08 15:36:00 +0300

    refactor petitions/urls.py

--- petitions/urls.py
+++ petitions/urls.py
@@ -17,10 +17,12 @@ urlpatterns = [
     path("create/", PetitionCreateView.as_view(), name="petition_create"),
     path("<int:pk>/delete/", delete_petition_view, name="petition_delete"),
     path("<int:pk>/", petition_detail_view, name="petition_detail"),
+    path("<int:pk>/support/", support_petition_view, name="support_petition"),
+    path("<int:pk>/status/", set_petition_status, name="petition_set_status"),
+    
     path("<int:pk>/comment/add/", add_comment_view, name="add_comment"),
     path("<int:petition_pk>/comment/<int:comment_pk>/edit/", edit_comment_view, name="edit_comment"),
-    path("<int:petition_pk>/comment/<int:comment_pk>/delete/", delete_comment_view, name="delete_comment"),
-    path("<int:pk>/support/", support_petition_view, name="support_petition"),
-    path("<int:pk>/status/", set_petition_status, name="petition_set_status")
+    path("<int:petition_pk>/comment/<int:comment_pk>/delete/", delete_comment_view, name="delete_comment")
+
 
 ]
